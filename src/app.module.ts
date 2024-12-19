727ab337cf30b340a2e26077da640b57b772481d
eldanielsoares <danielsoares7717@gmail.com>
2024-12-19 08:37:50 -0300

    chore: set redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [],
+  imports: [
+    RedisModule.forRoot({
+      type: 'single',
+      url: 'redis://redis:6379',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
