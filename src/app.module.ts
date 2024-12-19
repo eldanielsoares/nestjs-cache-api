a2269626260a1940d4382eb5f01bc1c63fa363be
eldanielsoares <danielsoares7717@gmail.com>
2024-12-19 10:17:12 -0300

    chore: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { PrismaService } from './modules/prisma/prisma-service';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
@@ -9,8 +11,9 @@ import { RedisModule } from '@nestjs-modules/ioredis';
       type: 'single',
       url: 'redis://redis:6379',
     }),
+    PostsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
